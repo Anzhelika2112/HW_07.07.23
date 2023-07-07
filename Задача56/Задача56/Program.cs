b603e805899ef5fc0cf8f5bc232e617856e4dc2b
Anzhelika <anjelichka2112@mail.ru>
2023-07-07 14:47:33 +0300

    Добавила задания

---
+++ Задача56/Задача56/Program.cs
@@ -0,0 +1,64 @@
+﻿//  Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+
+int minSumRowElements = MinSumRowElements(array); 
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MinSumRowElements(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    Console.Write($"Номер строки с наименьшей суммой элементов: {index + 1} строка"); 
+    return index;
+}
---
+++ Задача58/Задача58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+
+
+Console.Clear(); 
+
+int[,] array1 = GetArray(4, 4, 1, 10);
+int[,] array2 = GetArray(4, 4, 10, 20);
+
+PrintArray(array1);
+Console.WriteLine("---");
+PrintArray(array2);
+
+int[,] prod = ProdArray(array1, array2);
+Console.WriteLine("Результирующая матрица: ");
+PrintProdArray(prod); 
+
+
+int[,] GetArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(min, max); 
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ProdArray(int[,] array1, int[,] array2)
+{
+    int[,] prod = new int[array1.GetLength(0), array1.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            prod[i, j] = array1[i, j] * array2[i, j];
+        }
+    }
+    return prod;
+}
+
+void PrintProdArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
